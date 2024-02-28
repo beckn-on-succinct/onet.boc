7f2d3a9739238d7e3e9e1ad029acc9a9d5a88660
Venky <venkatramanm@gmail.com>
2024-02-28 18:51:50 +0530

    tag fix for 0.9

--- src/main/java/in/succinct/beckn/boc/Intent.java
+++ src/main/java/in/succinct/beckn/boc/Intent.java
@@ -3,15 +3,7 @@ package in.succinct.beckn.boc;
 import in.succinct.beckn.TagGroups;
 import in.succinct.beckn.Tags;
 
-public class Fulfillment extends in.succinct.beckn.Fulfillment {
-    public Fulfillment(){
-        super();
-    }
-    @Override
-    public boolean isExtendedAttributesDisplayed(){
-        return false;
-    }
-
+public class Intent extends in.succinct.beckn.Intent {
     @Override
     public void setTags(TagGroups tags) {
         Tags tags1 = new Tags();
