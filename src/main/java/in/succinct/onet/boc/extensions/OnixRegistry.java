c30d65657252628626aa71279125dcd6a5c4a0d8
Venky <venkatramanm@gmail.com>
2024-07-19 21:03:51 +0530

    Onix clean up

--- src/main/java/in/succinct/onet/boc/extensions/OnixRegistry.java
+++ src/main/java/in/succinct/onet/boc/extensions/OnixRegistry.java
@@ -3,15 +3,26 @@ package in.succinct.onet.boc.extensions;
 import com.venky.swf.routing.Config;
 import in.succinct.onet.core.adaptor.NetworkAdaptorFactory;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class OnixRegistry extends BocRegistry {
+    static final Set<String> WELL_KNOWN_NETWORKS = new HashSet<String>(){{
+        add("beckn_open");
+        add("hbo");
+        add("gex");
+        add("openamsterdam");
+    }};
+    static final String ONIX =  Config.instance().getProperty("in.succinct.onet.name","onix");
+
     static {
-        if (isEnabled(Config.instance().getProperty("in.succinct.onet.name","beckn_open"))) {
+        if (!WELL_KNOWN_NETWORKS.contains(ONIX) && isEnabled(ONIX)) {
             NetworkAdaptorFactory.getInstance().registerAdaptor(new OnixRegistry());
         }
     }
 
     public OnixRegistry(){
-        super(Config.instance().getProperty("in.succinct.onet.name","beckn_open"));
+        super(ONIX);
     }
 
 }
