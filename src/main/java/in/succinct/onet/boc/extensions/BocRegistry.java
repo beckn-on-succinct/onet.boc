4e4009f3d36276ae34b73ffbc4a380586314851c
Venky <venkatramanm@gmail.com>
2025-05-26 20:03:05 +0530

    Load domains more robustly

--- src/main/java/in/succinct/onet/boc/extensions/BocRegistry.java
+++ src/main/java/in/succinct/onet/boc/extensions/BocRegistry.java
@@ -2,25 +2,47 @@ package in.succinct.onet.boc.extensions;
 
 import com.venky.core.util.ObjectUtil;
 import com.venky.swf.db.annotations.column.ui.mimes.MimeType;
+import com.venky.swf.db.model.status.RouterStatus;
+import com.venky.swf.db.model.status.ServerStatus;
 import com.venky.swf.integration.api.Call;
 import com.venky.swf.integration.api.HttpMethod;
 import com.venky.swf.plugins.background.core.IOTask;
 import com.venky.swf.plugins.background.core.RetryTask;
+import com.venky.swf.plugins.background.core.Task;
 import com.venky.swf.plugins.background.core.TaskManager;
+import com.venky.swf.routing.Router;
 import in.succinct.onet.core.adaptor.NetworkAdaptor;
 import in.succinct.onet.core.adaptor.NetworkApiAdaptor;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.TimeoutException;
 
 public class BocRegistry extends NetworkAdaptor {
     protected BocRegistry(String networkName){
         super(networkName);
-        TaskManager.instance().executeAsync((IOTask)()->{
-            loadDomains();
-        },    false);
+        TaskManager.instance().executeAsync(
+                new Task() {
+                    @Override
+                    public void execute() {
+                        try {
+                            Thread.currentThread().wait(5000);
+                        }catch (InterruptedException ex){
+                            //
+                        }finally {
+                            ServerStatus serverStatus  = new ServerStatus();
+                            Router.instance().status(serverStatus);
+                            if (serverStatus.getRouterStatus() == RouterStatus.initialized) {
+                                loadDomains();
+                            }else if (serverStatus.getRouterStatus() != RouterStatus.failed){
+                                TaskManager.instance().executeAsync(this,false);
+                            }
+                        }
+                    }
+                },false);
+        
     }
 
     private NetworkApiAdaptor apiAdaptor;
@@ -53,6 +75,7 @@ public class BocRegistry extends NetworkAdaptor {
         if (areDomainsLoaded()){
             return;
         }
+        
 
         JSONArray array = new Call<JSONObject>().url(getBaseUrl(),"/network_domains").method(HttpMethod.GET).
                 headers(new HashMap<>(){{
